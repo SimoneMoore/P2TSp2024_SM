969a498153df74ed83026cd1dff608655d114d69
Simone <moores0929@gmail.com>
2024-08-19 00:07:48 -0400

    updated file structure, installed resources for icons and professional image. HTML reorganized. Incomplete

---
+++ Portfolio/src/Assets/logo.js
@@ -0,0 +1,5 @@
+import logo from "super-tiny-icons/images/svg/github.svg";
+
+<img src={logo} />;
+
+// react importing of tiny-icons
---
+++ Portfolio/src/Pages/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage= () => {
+    return (
+      <main className="home-container">
+       
+      </main>
+    )
+  }
+  
+  export default MainPage
